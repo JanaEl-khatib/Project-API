d762c4fc40b3852f5bf7726b3a370399a8aeaab6
JanaEl-khatib <jel-khat@my.loyno.edu>
2023-12-03 16:17:33 -0600

    Starting out

---
+++ index.js
@@ -0,0 +1,22 @@
+const { translate } = require('arajs/src/trans/utility')
+const express = require('express')
+const helmet = require('helmet')
+const app = express()
+const port = 3000
+
+app.use(helmet()) 
+
+let translate_count = 0
+
+app.get('/', (req, res) => {
+  res.json({translate_count});
+})
+
+app.post('/translation', (req, res) => {
+  translate_count++
+  res.status(201).send()
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
